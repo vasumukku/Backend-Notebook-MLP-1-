91ba528fbf5d559e94acc1209dade20a264f40ad
vasumukku <mukkuvasu01@gmail.com>
2026-05-28 12:39:22 +0530

    Added toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,72 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route,Navigate } from 'react-router'
+import React from 'react';
 
-import Login from './pages/Login'
-import Signin from './pages/Signin'
-import Notebook from './pages/Notebook'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+
+import { Toaster } from "react-hot-toast";
+
+import Login from './pages/Login';
+
+import Signin from './pages/Signin';
+
+import Notebook from './pages/Notebook';
 
 const App = () => {
+
+  const token =
+    localStorage.getItem("token");
+
   return (
+
     <div>
+
       <BrowserRouter>
+
+        {/* TOAST */}
+
+        <Toaster position="top-right" />
+
         <Routes>
 
-          <Route path="/" element={<Login />} /> 
-          <Route path="/signin" element={<Signin />} /> 
-          <Route path="/Notebook" element={<Notebook />} /> 
+          {/* LOGIN PAGE */}
 
-         
+          <Route
+            path="/"
+            element={
+              token
+                ? <Navigate to="/notebook" />
+                : <Login />
+            }
+          />
+
+          {/* SIGNIN PAGE */}
+
+          <Route
+            path="/signin"
+            element={<Signin />}
+          />
+
+          {/* PROTECTED NOTEBOOK PAGE */}
+
+          <Route
+            path="/notebook"
+            element={
+              token
+                ? <Notebook />
+                : <Navigate to="/" />
+            }
+          />
+
         </Routes>
+
       </BrowserRouter>
+
     </div>
   )
 }
 
-export default App
+export default App;
